c2b1e30fad1e43c2b4908367950acbd02b858b60
TG ⊗ Θ <*@tg-x.net>
2018-12-11 22:11:46 +0100

    use sunnyhash

--- lib/urps.mli
+++ lib/urps.mli
@@ -1,45 +1,33 @@
 (** URPS: Uniform Random Peer Sampling *)
 
-module UniHash2 : sig
-
-  type t
-
-  val init :
-    t
-
-  val hash :
-    t -> Int64.t
-    -> Int32.t
-
-end
-
 module Estimator : sig
 
   type t
 
   val init :
-    int -> int
+    int -> int -> int
     -> t
 
-  (** [init s k]
+  (** [init s k idlen]
 
       Initialize estimator:
 
       - [s]: precision of estimator (s = log(1/δ))
       - [k]: error of estimator (k = e/ε)
+      - [idlen]: maximum length of node ID string values
 
       The error of the estimator in answering a query for
       f_j is within a factor of ε with probability δ. *)
 
   val add :
-    t -> Int64.t
+    t -> string
     -> unit
   (** [add t j]
 
       Add node [j] to estimator [t] *)
 
   val estimate :
-    t -> Int64.t
+    t -> string
     -> int
   (** [estimate t j].
 
@@ -60,21 +48,26 @@ module Sampler : sig
   type t
 
   val init :
-    int -> int -> int
+    int -> int -> int -> int
     -> t
-  (** [init c s k]
+  (** [init c s k idlen]
       Initialize sampler with settings c, s, k:
 
       - [c]: sampler memory size (size of set Γ)
       - [s]: precision of estimator (s = log(1/δ))
       - [k]: error of estimator (k = e/ε)
+      - [idlen]: maximum length of node ID string values
 
       The error of the estimator in answering a query for
-      f_j is within a factor of ε with probability δ. *)
+      f_j is within a factor of ε with probability δ.
+
+      The sampler uses the [Random] module
+      which needs to be initialized before calling this function.
+   *)
 
   val add :
-    t -> Int64.t
-    -> Int64.t
+    t -> string
+    -> string
   (** [add t j]
       Add node [j] from input stream to sampler [t].
 
--- test/test_urps.ml
+++ test/test_urps.ml
@@ -2,13 +2,23 @@ open Urps
 open OUnit2
 open Printf
 
+let get_counter h j =
+  match Hashtbl.find_opt h j with
+  | Some c -> c
+  | None -> 0
+
+let get_counter_int h j =
+  match Hashtbl.find_opt h (Printf.sprintf "%d" j) with
+  | Some c -> c
+  | None -> 0
+
 let rec stats ?(i=0) ?(max_e=0.) n m sin sout =
   if i = 0
   then printf "\n\nn\tIN%%\tOUT%%\te%%\n\n";
   if i < n
   then
-    let pin  = (float_of_int sin.(i))  /. (float_of_int m) *. 100. in
-    let pout = (float_of_int sout.(i)) /. (float_of_int m) *. 100. in
+    let pin  = (float_of_int (get_counter_int sin i))  /. (float_of_int m) *. 100. in
+    let pout = (float_of_int (get_counter_int sout i)) /. (float_of_int m) *. 100. in
     let e = pout /. (100. /. (float_of_int n)) -. 1. in
     let max_e = if max_e < e then e else max_e in
     printf "%d\t%.2f\t%.2f\t%+.2f\n" i pin pout e;
@@ -21,15 +31,16 @@ let rec stats ?(i=0) ?(max_e=0.) n m sin sout =
 
 let test_sampler _ctx =
   let c = 700 in
-  let k = 50 in
   let s = 10 in
-  let smpl = Sampler.init c s k in
+  let k = 50 in
+  let idlen = 16 in
+  let smpl = Sampler.init c s k idlen in
   let m = 10000 in (* size of input stream *)
   let mp = 0.2 in (* malicious probability *)
   let n = 100 in (* population size *)
   let nm = 10 in (* maliccious population size *)
-  let sin = Array.make n 0 in
-  let sout = Array.make n 0 in
+  let sin = Hashtbl.create n in
+  let sout = Hashtbl.create n in
 
   (* add nodes *)
   for _ = 1 to m
@@ -40,12 +51,10 @@ let test_sampler _ctx =
       then nm  (* malicious *)
       else n
     in
-    let j = Random.int64 @@ Int64.of_int bound in
+    let j = Int64.to_string @@ Random.int64 @@ Int64.of_int bound in
     let k = Sampler.add smpl j in
-    let j = Int64.to_int j in
-    let k = Int64.to_int k in
-    sin.(j) <- sin.(j) + 1;
-    sout.(k) <- sout.(k) + 1;
+    Hashtbl.replace sin  j @@ (get_counter sin  j) + 1;
+    Hashtbl.replace sout k @@ (get_counter sout k) + 1;
   done;
 
   let max_e = stats n m sin sout in
