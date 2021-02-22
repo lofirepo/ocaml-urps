b964f88f102e31b6d90390b14824f6bd9bc485ef
TG × ⊙ <*@tg-x.net>
2021-02-23 00:43:42 +0100

    nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "URPS: Uniform Random Peer Sampler";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.ocaml-urps =
+      with import nixpkgs { system = "x86_64-linux"; };
+      ocamlPackages.buildDunePackage rec {
+        pname = "urps";
+        version = "0.0.1";
+        src = self;
+        useDune2 = true;
+
+        buildInputs = with pkgs.ocamlPackages; [
+          nocrypto
+        ];
+        nativeBuildInputs = with pkgs.ocamlPackages; [
+          ounit odoc
+        ];
+      };
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.ocaml-urps;
+  };
+}
