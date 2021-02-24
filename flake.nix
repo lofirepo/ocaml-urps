319c6ac5eda8c14595a6992090c33f249a818999
TG × ⊙ <*@tg-x.net>
2021-02-24 02:30:46 +0100

    nix flake: support multiple systems & ocaml version

--- flake.nix
+++ flake.nix
@@ -1,29 +1,79 @@
 {
   description = "URPS: Uniform Random Peer Sampler";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
-  };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux" "aarch64-linux" "armv7l-linux"
+        "x86_64-darwin" "aarch64-darwin"
+      ];
+      supportedOcamlPackages = [
+        "ocamlPackages_4_10"
+        "ocamlPackages_4_11"
+        "ocamlPackages_4_12"
+      ];
+      defaultOcamlPackages = "ocamlPackages_4_11";
 
-  outputs = { self, nixpkgs }: {
+      forAllOcamlPackages = nixpkgs.lib.genAttrs supportedOcamlPackages;
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor =
+        forAllSystems (system:
+          import nixpkgs {
+            inherit system;
+            overlays = [ self.overlay ];
+          });
+    in
+      {
+        overlay = final: prev:
+          with final;
+          let mkOcamlPackages = prevOcamlPackages:
+                with prevOcamlPackages;
+                let ocamlPackages =
+                      {
+                        inherit ocaml;
+                        inherit findlib;
+                        inherit ocamlbuild;
+                        inherit opam-file-format;
+                        inherit buildDunePackage;
 
-    packages.x86_64-linux.ocaml-urps =
-      with import nixpkgs { system = "x86_64-linux"; };
-      ocamlPackages.buildDunePackage rec {
-        pname = "urps";
-        version = "0.0.1";
-        src = self;
-        useDune2 = true;
+                        urps =
+                          buildDunePackage rec {
+                            pname = "urps";
+                            version = "0.0.1";
+                            src = self;
 
-        buildInputs = with pkgs.ocamlPackages; [
-          nocrypto
-        ];
-        nativeBuildInputs = with pkgs.ocamlPackages; [
-          odoc
-          ounit
-        ];
+                            useDune2 = true;
+                            doCheck = true;
+
+                            nativeBuildInputs = with ocamlPackages; [
+                              odoc
+                              ounit
+                            ];
+                            buildInputs = with ocamlPackages; [
+                              nocrypto
+                            ];
+                          };
+                      };
+                in ocamlPackages;
+          in
+            let allOcamlPackages =
+                  forAllOcamlPackages (ocamlPackages:
+                    mkOcamlPackages ocaml-ng.${ocamlPackages}
+                  );
+            in
+              allOcamlPackages // {
+                ocamlPackages = allOcamlPackages.${defaultOcamlPackages};
+              };
+
+        packages =
+          forAllSystems (system:
+            forAllOcamlPackages (ocamlPackages:
+              nixpkgsFor.${system}.${ocamlPackages}
+            ));
+
+        defaultPackage =
+          forAllSystems (system:
+            nixpkgsFor.${system}.ocamlPackages.urps
+          );
       };
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.ocaml-urps;
-  };
 }
